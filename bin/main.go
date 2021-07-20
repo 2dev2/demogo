796ef9bc686398407f4214f070fe630b026e9e3f
Dharmadev K <dhak@groupon.com>
2021-07-20 13:37:19 +0530

    ok

--- bin/main.go
+++ bin/main.go
@@ -1,9 +1,10 @@
 package bin
 
 import (
+	"fmt"
 	"github.com/2dev2/demogo/service"
 )
 
 func main(){
-
+	fmt.Print("dev")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
 module github.com/2dev2/demogo
 
+replace github.com/2dev2/demogo => ../
 go 1.16
